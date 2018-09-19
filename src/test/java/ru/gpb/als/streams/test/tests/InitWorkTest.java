84a2fd64118276447a7927737cee33f5d8904d97
Boris Zhguchev <zhguchev@gmail.com>
2018-09-19 10:45:13 +0300

    add test

--- src/test/java/ru/gpb/als/streams/test/tests/InitWorkTest.java
+++ src/test/java/ru/gpb/als/streams/test/tests/InitWorkTest.java
@@ -5,6 +5,7 @@ import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.junit.Assert;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,11 +14,15 @@ import ru.gpb.als.model.Tuple1;
 import ru.gpb.als.streams.test.BaseStreamsTest;
 import ru.gpb.als.streams.test.StreamsTestHelper;
 import ru.gpb.als.streams.test.data.StreamsUtils;
+import ru.gpb.als.streams.test.helpers.StreamsTestHelperContext;
 import ru.gpb.als.streams.test.helpers.generators.AvroGenerator;
 
 import java.util.Optional;
 import java.util.Properties;
 
+import static org.junit.Assert.*;
+import static ru.gpb.als.streams.test.data.StreamsUtils.*;
+import static ru.gpb.als.streams.test.data.StreamsUtils.H.*;
 import static ru.gpb.als.streams.test.helpers.ValueProducer.NULL_PRODUCER;
 
 /**
@@ -30,57 +35,57 @@ public class InitWorkTest extends BaseStreamsTest {
   @Autowired
   @Qualifier("streams")
   private Properties properties;
-
-
-  @Test
-  public void shouldLastWinTest() {
-    Optional<Country> country =
-        StreamsTestHelper.run(builder, properties)
-            .sender("internal.country", NULL_PRODUCER, this::newCountryWithFixAskId)
-            .send(20)
-            .pipe()
-            .keeper("internal.country_group_by_ask", Tuple1.class, Country.class)
-            .find(StreamsUtils.H.wrap("1"));
-
-    Assert.assertTrue(country.isPresent());
-
-    --sequencer;
-    Assert.assertEquals(newCountryWithFixAskId(), country.get());
-
-  }
-
-  @Test
-  public void testAvroGenerator() {
-    ConsumerRecord<Tuple1, Country> last =
-        StreamsTestHelper.run(builder, properties)
-            .sender("internal.country", Tuple1.class, Country.class)
-            .send(5).last();
-
-    Optional<Country> country =
-        StreamsTestHelper.run(builder, properties)
-            .keeper("internal.country_group_by_ask", Tuple1.class, Country.class)
-            .find(StreamsUtils.H.wrap(last.value().getAskId()));
-
-    Assert.assertTrue(country.isPresent());
-
-    --sequencer;
-    Assert.assertEquals(last.value(), country.get());
-
-  }
-
-
   private int sequencer = 0;
 
+  private String store = "internal.country_group_by_ask";
+  private String stream = "internal.country";
+
+  @Test
+  public void shouldLastWinWithManuallyTest() {
+
+	Optional<Country> countryOpt =
+	  StreamsTestHelper.run(builder, properties)
+		.sender(stream, NULL_PRODUCER, this::newCountryWithFixAskId)
+		.send(5)
+		.pipe()
+		.keeper(store, Tuple1.class, Country.class)
+		.find(wrap("1"));
+	--sequencer;
+
+	assertTrue(countryOpt.isPresent());
+	assertEquals(newCountryWithFixAskId(), countryOpt.get());
+
+  }
+
+  @Test
+  public void shouldLastWinWithGeneratorTest() {
+	StreamsTestHelperContext ctx = StreamsTestHelper.run(builder, properties);
+
+	Country last =
+	  ctx
+		.sender(stream, Tuple1.class, Country.class)
+		.send(5).last().value();
+
+	Optional<Country> countryOpt =
+	  ctx
+		.keeper(store, Tuple1.class, Country.class)
+		.find(wrap(last.getAskId()));
+
+	assertTrue(countryOpt.isPresent());
+	assertEquals(last, countryOpt.get());
+
+  }
+
   private Country newCountryWithFixAskId() {
-    int next = ++sequencer;
-    return
-        Country
-            .newBuilder()
-            .setId(next)
-            .setAskId(1)
-            .setName("name" + next)
-            .setSadkoId(next)
-            .build();
+	int next = ++sequencer;
+	return
+	  Country
+		.newBuilder()
+		.setId(next)
+		.setAskId(1)
+		.setName("name" + next)
+		.setSadkoId(next)
+		.build();
   }
 
 }
